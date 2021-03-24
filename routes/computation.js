615d9482555124fdc52565a0d2e89a532eaf54f2
Tejaswi Nallavolu <tejaswireddynallavolu@gmail.com>
2021-03-24 16:54:37 -0500

    added user.js

--- routes/computation.js
+++ routes/computation.js
@@ -1,12 +1,15 @@
 var express = require('express');
+const url = require('url');
 var router = express.Router();
+const querystring = require('querystring');
+let alert = require('alert');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  let randomno=Math.floor(Math.exp()*100)+1
+  var a = req.query.value; 
 
-  res.render('compute',{result:'exptential math function '+randomno+ 'is '+Math.exp(randomno)})
-
+  res.send('ceil applied to: ' + a + " is " + Math.ceil(a));
 });
 
 module.exports = router;
+
